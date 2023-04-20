16a92888559e300f198d90ef44602a9f26ee1a0e
Adam Lancaster <lancaster28@hotmail.com>
2023-04-19 19:07:14 -0500

    Working Mov with more exceptions
    This implements mov for when we have displacements - both 8 bit and 16
    bit. Also implements different kinds of mov like from and too memory
    registers and instructions

--- lib/virtual8086.ex
+++ lib/virtual8086.ex
@@ -2,34 +2,85 @@ defmodule Virtual8086 do
   @moduledoc """
   A virtual 8086 CPU chip developed as part of the performance aware programming course.
 
-  This is the schema for the mov instruction
-    <<opcode::6, d_bit::1, w_bit::1, mod::2, reg::3, rm::3>>
-  """
-  def to_asm(<<>>, asm), do: asm
-
-  def to_asm(<<instruction::bytes-size(2), rest::binary>>, asm) do
-    to_asm(rest, asm <> instruction(instruction) <> "\n")
-  end
-
-  @doc """
-  This is the schema for the mov instruction:
+  This is the schema for the mov to_asm:
 
       <<opcode::6, d_bit::1, w_bit::1, mod::2, reg::3, rm::3>>
 
   First 6 bits is the opcode. For register to/from register mov it is 100010
   """
-  def instruction(<<0b100010::6, d_bit::1, w_bit::1, 0b11::2, reg::3, rm::3>>) do
-    {source, destination} =
-      case {d_bit, w_bit} do
-        {0, 1} -> {sixteen_bit_registers(reg), sixteen_bit_registers(rm)}
-        {0, 0} -> {eight_bit_registers(reg), eight_bit_registers(rm)}
-        {1, 0} -> {eight_bit_registers(rm), eight_bit_registers(reg)}
-        {1, 1} -> {sixteen_bit_registers(rm), sixteen_bit_registers(reg)}
-      end
+  def to_asm(<<>>, asm), do: asm
 
-    # Obviously horrible that destination comes first.
-    "mov" <> " " <> destination <> ", " <> source
+  # To / From Register:
+  def to_asm(<<0b100010::6, d_bit::1, w_bit::1, 0b11::2, reg::3, rm::3, rest::binary>>, asm) do
+    {source, destination} = {register(reg, w_bit), register(rm, w_bit)} |> order_by(d_bit)
+    to_asm(rest, asm <> "mov #{destination}, #{source}\n")
   end
+
+  def to_asm(
+        <<0b100010::6, d_bit::1, w_bit::1, 0b00::2, reg::3, 0b110::3,
+          addr::unsigned-little-integer-size(16), rest::binary>>,
+        asm
+      ) do
+    {source, destination} = {register(reg, w_bit), "[#{addr}]"} |> order_by(d_bit)
+    to_asm(rest, asm <> "mov #{destination}, #{source}\n")
+  end
+
+  def to_asm(<<0b100010::6, d_bit::1, w_bit::1, 0b00::2, reg::3, rm::3, rest::binary>>, asm) do
+    {source, destination} = {register(reg, w_bit), "[#{effective_addr(rm)}]"} |> order_by(d_bit)
+    to_asm(rest, asm <> "mov #{destination}, #{source}\n")
+  end
+
+  def to_asm(
+        <<0b100010::6, d_bit::1, w_bit::1, 0b01::2, reg::3, rm::3,
+          low_disp::signed-little-integer-size(8), rest::binary>>,
+        asm
+      ) do
+    rm = "[#{effective_addr(rm)}#{sign(low_disp)}]"
+    {source, destination} = {register(reg, w_bit), rm} |> order_by(d_bit)
+    to_asm(rest, asm <> "mov #{destination}, #{source}\n")
+  end
+
+  def to_asm(
+        <<0b100010::6, d_bit::1, w_bit::1, 0b10::2, reg::3, rm::3, displacement::16-little,
+          rest::binary>>,
+        asm
+      ) do
+    rm = "[#{effective_addr(rm)} + #{displacement}]"
+    {source, destination} = {register(reg, w_bit), rm} |> order_by(d_bit)
+    to_asm(rest, asm <> "mov #{destination}, #{source}\n")
+  end
+
+  def to_asm(
+        <<0b1011::4, 0::1, reg::3, immediate::8, rest::binary>>,
+        asm
+      ) do
+    to_asm(rest, asm <> "mov #{register(reg, 0)}, #{immediate}\n")
+  end
+
+  def to_asm(
+        <<0b1011::4, 1::1, reg::3, immediate::16-little, rest::binary>>,
+        asm
+      ) do
+    to_asm(rest, asm <> "mov #{register(reg, 1)}, #{immediate}\n")
+  end
+
+  def sign(0), do: ""
+  def sign(disp), do: if(disp > 0, do: " + #{abs(disp)}", else: " - #{abs(disp)}")
+
+  defp effective_addr(0b000), do: "bx + si"
+  defp effective_addr(0b001), do: "bx + di"
+  defp effective_addr(0b010), do: "bp + si"
+  defp effective_addr(0b011), do: "bp + di"
+  defp effective_addr(0b100), do: "si"
+  defp effective_addr(0b101), do: "di"
+  defp effective_addr(0b110), do: "bp"
+  defp effective_addr(0b111), do: "bx"
+
+  defp order_by({source, dest}, 0), do: {source, dest}
+  defp order_by({source, dest}, 1), do: {dest, source}
+
+  defp register(location, 1), do: sixteen_bit_registers(location)
+  defp register(location, 0), do: eight_bit_registers(location)
 
   defp sixteen_bit_registers(0), do: "ax"
   defp sixteen_bit_registers(1), do: "cx"
--- test/virtual8086_test.exs
+++ test/virtual8086_test.exs
@@ -17,4 +17,28 @@ defmodule Virtual8086Test do
       assert Virtual8086.to_asm(binary, "") == answer
     end
   end
+
+  describe "8 bit displacement" do
+    test "we can do 8 bit displacements thank you very much" do
+      binary = <<0b100010::6, 0b0::1, 0b0::1, 0b01::2, 0b000::3, 0b000::3, 0b11111111::8>>
+      assert Virtual8086.to_asm(binary, "") == "mov [bx + si - 1], al\n"
+    end
+
+    test "special case for mod of 00" do
+      binary =
+        <<0b100010::6, 0b0::1, 0b0::1, 0b00::2, 0b001::3, 0b110::3, 0b0101010111111111::16>>
+
+      assert Virtual8086.to_asm(binary, "") == "mov [65365], cl\n"
+    end
+
+    test "Listing 40" do
+      binary =
+        "../computer_enhance/perfaware/part1/listing_0039_more_movs"
+        |> Path.expand()
+        |> File.read!()
+
+      asm = Virtual8086.to_asm(binary, "")
+      File.write!("./output.asm", asm)
+    end
+  end
 end
